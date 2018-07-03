40f180f337335f21dcecffc59ad56365762abdc5
Scoti Dodson <scotidodson@gmail.com>
2018-07-03 03:36:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// add solution here
+function theBeatlesPlay(beatlesMember, beatlesInstrument){
+  var beatles = [];
+  for(let i = 0; i<4; i++) {
+    beatles.push(`${beatlesMember[i]} plays ${beatlesInstrument[i]}`);
+  }
+  return beatles;
+}
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+
+function johnLennonFacts(facts){
+  do {console.log(facts + "!!!");
+  }
+  while(facts.length>0);
+  return facts;
+}
+
